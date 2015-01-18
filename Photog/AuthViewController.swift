40ccd2978ff8c076106d3e5b0a888a2ece7793f5
Andrew Patton <andrew@acusti.ca>
2015-01-18 16:36:40 -0500

    Add email / password text field with image icons

--- Photog/AuthViewController.swift
+++ Photog/AuthViewController.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    
+    // Other variables scoped to AuthViewController class
+    // ...
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.navigationController!.navigationBarHidden = false
+        // Define layout to exclude nav bar at top of screen
+        self.edgesForExtendedLayout = UIRectEdge.None
+        // Title
+        //self.navigationItem.title = "Log in"
+        
+        var emailImageView         = UIImageView(frame: CGRectMake(0, 0, 50, self.textEmail!.frame.size.height))
+        emailImageView.image       = UIImage(named: "IconEmail")
+        emailImageView.contentMode = .Center
+        
+        self.textEmail.leftView     = emailImageView
+        self.textEmail.leftViewMode = .Always
+        
+        var passwordImageView         = UIImageView(frame: CGRectMake(0, 0, 50, self.textPassword!.frame.size.height))
+        passwordImageView.image       = UIImage(named: "IconPassword")
+        passwordImageView.contentMode = .Center
+        
+        self.textPassword.leftView     = passwordImageView
+        self.textPassword.leftViewMode = .Always
+    }
+    
+    override func viewWillAppear(isAnimated: Bool) {
+        super.viewWillAppear(isAnimated)
+        
+        self.navigationController!.setNavigationBarHidden(false, animated: isAnimated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.textEmail.becomeFirstResponder()
     }
 
+    override func viewWillDisappear(isAnimated: Bool) {
+        super.viewWillDisappear(isAnimated)
+        
+        self.navigationController!.setNavigationBarHidden(true, animated: isAnimated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
